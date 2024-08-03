c6edda5e94de6716a86032b01b7034f8b0da029f
Yuvraj kumar <115492314+yuvrajkumar12@users.noreply.github.com>
2024-08-03 09:54:53 +0530

    udate

--- STL_Template_Library/07_set_c++/01_celing_of_every_item_on_right.cpp
+++ STL_Template_Library/07_set_c++/01_celing_of_every_item_on_right.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-void wceling1(vector<int>&nums){
+void wceling1(vector<int>&nums){//time complicity is o(n^2)
     int s=nums.size();
     for(int i=0;i<s;i++){
         int diff=INT_MAX;
@@ -19,6 +19,25 @@ void wceling1(vector<int>&nums){
     }
     cout<<endl;
 }
+void priorityceling(vector<int>&nums){//using set time complicity is O(n)
+    set<int>s;
+    int n=nums.size();
+    int res[n];
+    for(int i=n-1;i>=0;i--){
+        auto it=s.lower_bound(nums[i]);
+        if(it==s.end()){
+            res[i]=-1;
+        }
+        else{
+            res[i]=*it;
+        }
+        s.insert(nums[i]);
+    }
+    for(int i=0;i<n;i++){
+        cout<<res[i]<<" ";
+    }
+    cout<<endl;
+}
 int main(){
     string line;
     getline(cin,line);
@@ -29,5 +48,6 @@ int main(){
         nums.push_back(num);
     }
     wceling1(nums);
+    priorityceling(nums);
     return 0;
 }
